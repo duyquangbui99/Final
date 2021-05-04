52388a074edc839f04a1c3346504786ce731ab18
duyquangbui99 <77463586+duyquangbui99@users.noreply.github.com>
2021-05-04 15:43:30 -0500

    Update Exit Scene

---
+++ Assets/Scripts/lastScoreLive.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class lastScoreLive : MonoBehaviour
+{
+    public Text lastScore;
+    public Text lastLive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastScore.text = "SCORE: " + SaveData.score;
+        lastLive.text = "LIVE: " + SaveData.live;
+    }
+}
